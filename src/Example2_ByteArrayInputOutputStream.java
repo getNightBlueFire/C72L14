47d831fbaf527808330188c9dccf759b05fbabfc
Dmitry Hotko <d.hotko@invento.by>
2023-12-14 14:54:41 +0300

    commit

--- src/Example2_ByteArrayInputOutputStream.java
+++ src/Example2_ByteArrayInputOutputStream.java
@@ -1,3 +1,7 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -5,90 +9,101 @@ import java.io.IOException;
 public class Example2_ByteArrayInputOutputStream {
 	public static void main (String[] args) {
 
-		fileOutputStream();
-		fileInputStream();
-		fileInputStream2();
+
 	}
 
-	private static void fileOutputStream () {
+	private static void output () {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		String text = "Hello Wolrd!";
+		byte[] buffer = text.getBytes();
+		try{
+			baos.write(buffer);
+		}
+		catch(Exception ex){
+
+			System.out.println(ex.getMessage());
+		}
+		// превращаем массив байтов в строку
+		System.out.println(baos.toString());
+
+		// получаем массив байтов и выводим по символьно
+		byte[] array = baos.toByteArray();
+		for(byte b: array){
+
+			System.out.print((char)b);
+		}
+		System.out.println();
+	}
+
+	private static void writeTo () {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		String text = "Hello Wolrd!";
+		byte[] buffer = text.getBytes();
+		try{
+			baos.write(buffer);
+		}
+		catch(Exception ex){
+
+			System.out.println(ex.getMessage());
+		}
+		try(FileOutputStream fos = new FileOutputStream("hello.txt")){
+
+			baos.writeTo(fos);
+		}
+		catch(IOException e){
+
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private static void input () {
+		byte[] array1 = new byte[]{1, 3, 5, 7};
+		ByteArrayInputStream byteStream1 = new ByteArrayInputStream(array1);
+		int b;
+		while((b=byteStream1.read())!=-1){
+
+			System.out.println(b);
+		}
+
+		String text = "Hello world!";
+		byte[] array2 = text.getBytes();
+		// считываем 5 символов
+		ByteArrayInputStream byteStream2 = new ByteArrayInputStream(array2, 0, 5);
+		int c;
+		while((c=byteStream2.read())!=-1){
+
+			System.out.println((char)c);
+		}
+	}
+
+	private static void bais(){
+		String text = "Hello world!";
+		byte[] buffer = text.getBytes();
+		ByteArrayInputStream in = new ByteArrayInputStream(buffer);
+
+		try(BufferedInputStream bis = new BufferedInputStream(in)){
+
+			int c;
+			while((c=bis.read())!=-1){
+
+				System.out.print((char)c);
+			}
+		}
+		catch(Exception e){
+
+			System.out.println(e.getMessage());
+		}
+		System.out.println();
+	}
+
+	private static void bos(){
 		String text = "Hello world!"; // строка для записи
-		try (FileOutputStream fos = new FileOutputStream("notes.txt")) {
+		try(FileOutputStream out=new FileOutputStream("notes.txt");
+			BufferedOutputStream bos = new BufferedOutputStream(out))
+		{
 			// перевод строки в байты
 			byte[] buffer = text.getBytes();
-
-			fos.write(buffer, 0, buffer.length);
-			System.out.println("The file has been written");
-		} catch (IOException ex) {
-
-			System.out.println(ex.getMessage());
-		}
-	}
-
-	private static void fileInputStream () {
-		try (FileInputStream fin = new FileInputStream("notes.txt")) {
-			int i;
-			while ((i = fin.read()) != -1) {
-
-				System.out.print((char) i);
-			}
-		} catch (IOException ex) {
-
-			System.out.println(ex.getMessage());
-		}
-	}
-
-	private static void fileInputStream2 () {
-		try (FileInputStream fin = new FileInputStream("notes.txt")) {
-			byte[] buffer = new byte[256];
-			System.out.println("File data:");
-
-			int count;
-			while ((count = fin.read(buffer)) != -1) {
-
-				for (int i = 0; i < count; i++) {
-
-					System.out.print((char) buffer[i]);
-				}
-			}
-		} catch (IOException ex) {
-
-			System.out.println(ex.getMessage());
-		}
-	}
-
-	private static void fileInputStreamWithClose () {
-		FileInputStream fin = null;
-		try {
-			fin = new FileInputStream("notes.txt");
-
-			int i = -1;
-			while ((i = fin.read()) != -1) {
-
-				System.out.print((char) i);
-			}
-		} catch (IOException ex) {
-
-			System.out.println(ex.getMessage());
-		} finally {
-
-			try {
-
-				if (fin != null)
-					fin.close();
-			} catch (IOException ex) {
-
-				System.out.println(ex.getMessage());
-			}
-		}
-	}
-	private static void fileInputStreamWithCloseNew (){
-		try(FileInputStream fin=new FileInputStream("notes.txt"))
-		{
-			int i=-1;
-			while((i=fin.read())!=-1){
-
-				System.out.print((char)i);
-			}
+			bos.write(buffer, 0, buffer.length);
 		}
 		catch(IOException ex){
 
--- src/Example3_Stream.java
+++ src/Example3_Stream.java
@@ -1,98 +1,37 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 
 public class Example3_Stream {
 	public static void main (String[] args) {
-
-		fileOutputStream();
-		fileInputStream();
-		fileInputStream2();
+		ps_toFile();
 	}
 
-	private static void fileOutputStream () {
-		String text = "Hello world!"; // строка для записи
-		try (FileOutputStream fos = new FileOutputStream("notes.txt")) {
-			// перевод строки в байты
-			byte[] buffer = text.getBytes();
-
-			fos.write(buffer, 0, buffer.length);
-			System.out.println("The file has been written");
-		} catch (IOException ex) {
-
-			System.out.println(ex.getMessage());
-		}
-	}
-
-	private static void fileInputStream () {
-		try (FileInputStream fin = new FileInputStream("notes.txt")) {
-			int i;
-			while ((i = fin.read()) != -1) {
-
-				System.out.print((char) i);
-			}
-		} catch (IOException ex) {
-
-			System.out.println(ex.getMessage());
-		}
-	}
-
-	private static void fileInputStream2 () {
-		try (FileInputStream fin = new FileInputStream("notes.txt")) {
-			byte[] buffer = new byte[256];
-			System.out.println("File data:");
-
-			int count;
-			while ((count = fin.read(buffer)) != -1) {
-
-				for (int i = 0; i < count; i++) {
-
-					System.out.print((char) buffer[i]);
-				}
-			}
-		} catch (IOException ex) {
-
-			System.out.println(ex.getMessage());
-		}
-	}
-
-	private static void fileInputStreamWithClose () {
-		FileInputStream fin = null;
-		try {
-			fin = new FileInputStream("notes.txt");
-
-			int i = -1;
-			while ((i = fin.read()) != -1) {
-
-				System.out.print((char) i);
-			}
-		} catch (IOException ex) {
-
-			System.out.println(ex.getMessage());
-		} finally {
-
-			try {
-
-				if (fin != null)
-					fin.close();
-			} catch (IOException ex) {
-
-				System.out.println(ex.getMessage());
-			}
-		}
-	}
-	private static void fileInputStreamWithCloseNew (){
-		try(FileInputStream fin=new FileInputStream("notes.txt"))
+	private static void ps_toFile(){
+		try(PrintStream printStream = new PrintStream("notes3.txt"))
 		{
-			int i=-1;
-			while((i=fin.read())!=-1){
+			printStream.print("Hello World!");
+			printStream.println("Welcome to Java!");
 
-				System.out.print((char)i);
-			}
+			printStream.printf("Name: %s Age: %d \n", "Tom", 34);
+
+			String message = "PrintStream";
+			byte[] message_toBytes = message.getBytes();
+			printStream.write(message_toBytes);
+
+			System.out.println("The file has been written");
 		}
 		catch(IOException ex){
 
 			System.out.println(ex.getMessage());
+		}
+	}
+	private static void pw(){
+		try(PrintWriter pw = new PrintWriter(System.out))
+		{
+			pw.println("Hello world!");
 		}
 	}
 }
